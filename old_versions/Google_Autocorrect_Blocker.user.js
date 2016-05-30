5cd0276f968f06853ed43bfa1042a39a4d232490
TakaDaka. (@djtkdk_086969) <djtkdk-086969@users.noreply.github.com>
2016-05-30 17:51:27 +0900

    Old versions: jQuery-less version

--- old_versions/Google_Autocorrect_Blocker.user.js
+++ old_versions/Google_Autocorrect_Blocker.user.js
@@ -1,33 +1,32 @@
 // ==UserScript==
-// @author      たかだか。(TakaDaka.)
-// @name        Google Autocorrect Blocker
+// @author         たかだか。(TakaDaka.)
+// @name           Google Autocorrect Blocker
 // @name:en        Google Autocorrect Blocker
-// @namespace   https://twitter.com/djtkdk_086969
-// @description Googleの検索語句自動訂正を無効化します。
+// @namespace      https://twitter.com/djtkdk_086969
+// @description    Googleの検索語句自動訂正を無効化します。
 // @description:en Disables Google Search Autocorrect.
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @include     *://*.google.*/*
-// @exclude     *tbm=shop*
-// @exclude     *tbm=isch*
-// @exclude     *tbm=vid*
-// @exclude     *://plus.*
-// @exclude     *://mail.*
-// @exclude     *://productforums.*
-// @exclude     *://maps.*
-// @version     0.0.0.003
-// @grant       none
+// @include        *://*.google.*/*
+// @exclude        *tbm=shop*
+// @exclude        *tbm=isch*
+// @exclude        *tbm=vid*
+// @exclude        *://plus.*
+// @exclude        *://mail.*
+// @exclude        *://productforums.*
+// @exclude        *://maps.*
+// @version        0.0.0.004
+// @grant          none
+// @compatible     firefox, chrome
 // ==/UserScript==
 
 (function() {
-    console.log("GAB: Started.");
-    //console.log("GAB: At " + location.href);
-    if($("p.sp_cnt > a.spell_orig").length > 0) {
-        console.log("GAB: Autocorrect detected!");
+    console.log("GAB " + GM_info.script.version + " Started.");
+    var spell_orig = document.querySelector("p.sp_cnt > a.spell_orig");
+    if(spell_orig !== null) {
+        console.log("GAB: Autocorrect detected! Redirecting to your original search query...");
         var current_url_split = location.href.split("/");
-        var autocorrect_orig_url = $("p.sp_cnt > a.spell_orig:first").attr("href");
+        var autocorrect_orig_url = spell_orig.getAttribute("href");
         var new_url = current_url_split[0] + "//" + current_url_split[2] + autocorrect_orig_url;
-        //alert("GAB: Autocorrect detected! Redirecting to " + new_url);
         location.href = new_url;
-        return;
-    }
+    } else {
+        console.log("GAB: The results for your original search query is shown.");    }
 })();
